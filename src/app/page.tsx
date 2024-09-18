aa1f300b4509479612d44e023a1afd5e4c034db4
mdnayab <mdnayabkarim141@gmail.com>
2024-09-19 00:29:03 +0530

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <h1 className="text-2xl text-center">Nayab</h1>
+    </main>
+  );
+}
